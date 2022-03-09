a8dccf90324d379bbded1d2ddaa40d5b014d611c
Boris Kudryavtsev <boriskk.work@gmail.com>
2022-03-09 09:32:08 -0800

    Add soil sensor

--- sensor_test.py
+++ sensor_test.py
@@ -8,10 +8,9 @@ def main():
     adc = AdcExp.AdcExp()
 
     while True:
-      	a0 = adc.read_voltage(0)
-        print('Soil %d' % (a0))
-
-        print('Visible %03d UV %.2f IR %03d' % (SI1145.ReadVisible , SI1145.ReadUV/100 , SI1145.ReadIR),end=" ")
+        a0 = adc.read_voltage(0)
+        soil = (a0 / 5) * 100
+        print('Visible %03d | UV %.2f | IR %03d | Soil %f'  % (SI1145.ReadVisible , SI1145.ReadUV/100 , SI1145.ReadIR, a0), end=" ")
         print('\r', end='')
         time.sleep(0.5)
 
